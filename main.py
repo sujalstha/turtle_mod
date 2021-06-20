6b531c55432bcfd21c1cc355f208176ff4655f47
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-06-20 12:04:12 -0500

    Initial commit

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 import turtle
-from turtle import *
-
 
 tur = turtle.Turtle()
+x = 5
 
 # colors
 hot_pink = (119, 44, 75)
 dirty_blue = (52, 113, 124)
 dark_lime = (151, 192, 62)
+x = 10
 
-begin_fill()
-tur.speed(1)
-x = 5
-while x < 5:
+while x > 0:
     tur.forward(200)
     tur.left(170)
     x += 1
-end_fill()
 
 turtle.done()
